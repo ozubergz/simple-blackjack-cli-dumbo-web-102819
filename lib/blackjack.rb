6aaaea9acc3765162f67d029a5e60b0ec5e252cc
James Min <jmin499@gmail.com>
2019-10-29 04:55:30 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,11 +10,12 @@ end
 
 def display_card_total
   # code #display_card_total here
-  puts 
+  puts "Your cards add up to #{sum}"
 end
 
 def prompt_user
   # code #prompt_user here
+  
 end
 
 def get_user_input
